0457e0966ef8ec876109414e482a7cbaefa2d010
tega <beniyoke@gmail.com>
2024-06-05 16:54:58 +0100

    chore:Production setup

--- db/migrate/20240530164954_remove_datetime_from_quiz_duration.rb
+++ db/migrate/20240530164954_remove_datetime_from_quiz_duration.rb
@@ -1,6 +1,5 @@
 class RemoveDatetimeFromQuizDuration < ActiveRecord::Migration[7.1]
   def change
     add_column :quizzes, :quizzes_entries_count, :Integer, default: 0
-    add_column :quizzes, :duration, :Integer
   end
 end

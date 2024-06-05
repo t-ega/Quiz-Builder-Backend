db69446f5c88230b231fbe67dc9f9e272ada16ac
tega <beniyoke@gmail.com>
2024-06-05 19:04:00 +0100

    fix: Remove devise confrimation on signup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,9 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  # config.action_mailer.default_url_options = { host: "www.yourhost.com" }
+  config.action_mailer.default_url_options = {
+    host: ENV.fetch("PRODUCTION_HOST")
+  }
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
